af2a3d7fbf2339dfe3615c0ecee79f67b26ebd83
Marcelo Luis <marceloluis257gh@gmail.com>
2022-04-06 23:12:34 -0300

    Subindo primeiros arquivos

---
+++ target/debug/deps/rust_of_life-8987f104b217b124.d
@@ -0,0 +1,5 @@
+/home/marcelo/Documents/codigos/Rust/RustOfLife/target/debug/deps/rust_of_life-8987f104b217b124: src/main.rs
+
+/home/marcelo/Documents/codigos/Rust/RustOfLife/target/debug/deps/rust_of_life-8987f104b217b124.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_of_life.d
@@ -0,0 +1,1 @@
+/home/marcelo/Documents/codigos/Rust/RustOfLife/target/debug/rust_of_life: /home/marcelo/Documents/codigos/Rust/RustOfLife/src/main.rs
